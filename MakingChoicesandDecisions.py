a36390783384fc6b6262d160b9c3589223abdf9e
Zach <100073825+Z199Y@users.noreply.github.com>
2022-02-23 17:53:15 +0000

    Add files via upload
    Basic program using if and else statements

---
+++ MakingChoicesandDecisions.py
@@ -0,0 +1,21 @@
+userInput = input('Enter 1 or 2:')
+if userInput == '1':
+        print('What a world')
+        print('Universe is here')
+
+elif userInput == '2':
+        print('Python Rocks!')
+        print('I love Python')
+        
+        
+else:
+        print('You did not enter a valid number')
+
+        
+userInput = input ('How are you?:')
+if userInput == 'ok':
+        print('Cool')
+elif userInput == 'good':
+        print('Jolly Good')
+else:
+    input('Whats wrong?')
